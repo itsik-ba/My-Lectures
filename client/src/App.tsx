a2d07ac201652d88e2dcf513aa64ad13cf329903
itsik-ba <112100680+itsik-ba@users.noreply.github.com>
2023-07-06 14:09:48 +0300

    i

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,7 +10,7 @@ function App() {
       <Route path="/" element={<HomePage/>}/>
 
       
-</Routes>
+     </Routes>
     
       </div>
      
--- client/src/pages/homePage/HomePage.tsx
+++ client/src/pages/homePage/HomePage.tsx
@@ -2,7 +2,7 @@
 
 const HomePage = () => {
   return (
-    <div>HomexcvxcvxcvxxvxcxcvxcvxcvxcvxPage</div>
+    <div>barak</div>
   )
 }
 
