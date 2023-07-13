91eaa1511dd111f1ced75a11400a5314cf9ab9db
itsik-ba <112100680+itsik-ba@users.noreply.github.com>
2023-07-13 08:55:07 +0300

    icons

--- client/src/pages/mainBar/homePage/HomePage.tsx
+++ client/src/pages/mainBar/homePage/HomePage.tsx
@@ -61,7 +61,7 @@ const HomePage = () => {
           
          
          <div className="main__lectures">
-         <h2>חוגים</h2>
+         <h2>חוגים מומלצים</h2>
          <div className="main__lectures__showLec">
          <div className="main__lec">
           <div className="main__lec__img">
