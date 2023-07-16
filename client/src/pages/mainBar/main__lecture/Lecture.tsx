c8a9dc13c497fb2867d000520009138bbc0c7a46
itsik-ba <112100680+itsik-ba@users.noreply.github.com>
2023-07-16 19:41:09 +0300

    i

--- client/src/pages/mainBar/main__lecture/Lecture.tsx
+++ client/src/pages/mainBar/main__lecture/Lecture.tsx
@@ -4,24 +4,21 @@ import "./style/lecture.scss"
 const Lecture = () => {
   return (
     <div className="container">
-   
-      <div className="wrapper">
-
-        <div className="sectionA">
-          <div className="sectionA__secA"></div>
-          <div className="sectionA__secB"></div>
-          <div className="sectionA__secC"></div>
+     <div className="container__wrapper">
+      <div className="blockA">
+        <h2>xfsdfs</h2>
+        <div className="blockA__wrapperBlock">
+          <div className="a">ada</div>
+          <div className="a">ada</div>
+          <div className="a">ada</div>
         </div>
-
-        <div className="sectionB">
-          <div className="sectionB__secA"></div>
-          <div className="sectionB__secB"></div>
-          <div className="sectionB__secC"></div>
-        </div>
-
-        <div className="sectionC"></div>
-        <div className="sectionD"></div>
       </div>
+     
+     
+     </div>
+     
+      
+   
     </div>
   )
 }
