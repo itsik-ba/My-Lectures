46566d141096d79ad8b0f91ac5f766a1c7d5d369
itsik-ba <112100680+itsik-ba@users.noreply.github.com>
2023-07-19 14:48:17 +0300

    login some css

--- client/src/pages/topBar/top__login/Login.tsx
+++ client/src/pages/topBar/top__login/Login.tsx
@@ -15,11 +15,15 @@ const userLogin = () =>{
        <h2 className="login__h2login">התחברות</h2>
        <form className="login__myform" onSubmit={userLogin}>
         <label className="login__myform__label">דואר אלטרוני</label>
-        <input type="email" name="email"    className="login__myform__useremail" />
+        <input type="email" name="email"    className="login__myform__useremail" required />
         <label className="login__myform__label">סיסמה</label>
-        <input type="password"  name="password"  className="login__myform__userpassword"/>
+        <input type="password"  name="password"  className="login__myform__userpassword" required/>
           <button type="submit" className="login__myform__btn">התחבר</button>
-          <a href="http://">הרשמה</a>
+          <div className="forgotPassword">
+          <a href="http://" className="forgotPassword__link">הרשמה</a> 
+          <a href="http://" className="forgotPassword__link">שכחתי מייל/סיסמה</a> 
+          </div>
+         
        </form>
       </div>
 
