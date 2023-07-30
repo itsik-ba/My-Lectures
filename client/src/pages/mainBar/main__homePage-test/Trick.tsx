bbde572d95c2a0f28629615500ffbde6cca145c1
StMauda <120678111+StMauda@users.noreply.github.com>
2023-07-30 13:13:33 +0300

    order homepage

---
+++ client/src/pages/mainBar/main__homePage-test/Trick.tsx
@@ -0,0 +1,32 @@
+import TryMe from "./TryMe"
+
+
+
+const Trick = () => {
+  return (
+    <div>
+    <TryMe 
+      lecType="הרצאות מומלצות"
+      lecImg=""
+      lecName="sdfsdf"
+      lecInfo="xjhkdfjhfgkndfhgdfk"
+    />
+    <TryMe 
+      lecType="הרצאות מומלצות"
+      lecImg=""
+      lecName="sdfsdf"
+      lecInfo="xjhkdfjhfgkndfhgdfk"
+    />
+
+   
+
+    
+
+    
+
+
+    </div>
+  )
+}
+
+export default Trick
