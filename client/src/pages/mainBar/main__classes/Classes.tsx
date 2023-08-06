f38960e7d3eaa3735d885f8ddd068f6439ea2cc1
StMauda <120678111+StMauda@users.noreply.github.com>
2023-08-06 15:21:01 +0300

    order the type and the encoloure

--- client/src/pages/mainBar/main__classes/Classes.tsx
+++ client/src/pages/mainBar/main__classes/Classes.tsx
@@ -1,115 +1,13 @@
+import React from "react";
+import DataDisplay from "../encoloure/Enclosure"; 
 
-const Classes = () => {
-  return (
-    <div className="container">
-    <div className="container__wrapper">
-
-        <div className="blockA">
-         <span className="blockA__Sback"></span>
-         <span className="blockA__Sfrwd"></span>
-
-         <h2 className="blockA__h2">סיורים קרובים</h2>
-         <div className="blockA__wrapperBlock">
-         <div className="a">its aaa</div>
-         <div className="b">ada</div>
-         <div className="c">ada</div>
+const LectureMain: React.FC = () => {
+    return (
+        <div>
+            <DataDisplay dataType="tours" /> 
         </div>
-        </div>
+    );
+  };
   
-        <div className="blockA">
-        <span className="blockA__Sback"></span>
-         <span className="blockA__Sfrwd"></span>
-         <h2 className="blockA__h2">סיורים במרכז</h2>
-         <div className="blockA__wrapperBlock">
-         <div className="a">its aaa</div>
-         <div className="b">ada</div>
-         <div className="c">ada</div>
-        </div>
-        </div>
-
-        <div className="blockA">
-        <span className="blockA__Sback"></span>
-         <span className="blockA__Sfrwd"></span>
-         <h2 className="blockA__h2">סיורים בצפון</h2>
-         <div className="blockA__wrapperBlock">
-         <div className="a">its aaa</div>
-         <div className="b">ada</div>
-         <div className="c">ada</div>
-        </div>
-        </div>
-        
-        <div className="blockA">
-        <span className="blockA__Sback"></span>
-         <span className="blockA__Sfrwd"></span>
-         <h2 className="blockA__h2"> סיורים בדרום </h2>
-         <div className="blockA__wrapperBlock">
-         <div className="a">its aaa</div>
-         <div className="b">ada</div>
-         <div className="c">ada</div>
-        </div>
-        </div>
-
-        <div className="blockA">
-        <span className="blockA__Sback"></span>
-         <span className="blockA__Sfrwd"></span>
-         <h2 className="blockA__h2">סיורים רומנטיים</h2>
-         <div className="blockA__wrapperBlock">
-         <div className="a">its aaa</div>
-         <div className="b">ada</div>
-         <div className="c">ada</div>
-        </div>
-        </div>
-
-        <div className="blockA">
-        <span className="blockA__Sback"></span>
-         <span className="blockA__Sfrwd"></span>
-         <h2 className="blockA__h2">סיורים למשפחות</h2>
-         <div className="blockA__wrapperBlock">
-         <div className="a">its aaa</div>
-         <div className="b">ada</div>
-         <div className="c">ada</div>
-        </div>
-        </div>
-
-        <div className="blockA">
-        <span className="blockA__Sback"></span>
-         <span className="blockA__Sfrwd"></span>
-         <h2 className="blockA__h2">סיורים עם רכב , אופניים</h2>
-         <div className="blockA__wrapperBlock">
-         <div className="a">its aaa</div>
-         <div className="b">ada</div>
-         <div className="c">ada</div>
-        </div>
-        </div>
-
-        <div className="blockA">
-        <span className="blockA__Sback"></span>
-         <span className="blockA__Sfrwd"></span>
-         <h2 className="blockA__h2">סיורי תרבות, היסטוריה</h2>
-         <div className="blockA__wrapperBlock">
-         <div className="a">its aaa</div>
-         <div className="b">ada</div>
-         <div className="c">ada</div>
-        </div>
-        </div>
-
-        <div className="blockA">
-        <span className="blockA__Sback"></span>
-         <span className="blockA__Sfrwd"></span>
-         <h2 className="blockA__h2">סיורים בערב</h2>
-         <div className="blockA__wrapperBlock">
-         <div className="a">its aaa</div>
-         <div className="b">ada</div>
-         <div className="c">ada</div>
-        </div>
-        </div>
+  export default LectureMain;
   
-  
-  </div>
-  
-   
- </div>
-  )
-}
-
-export default Classes
--- client/src/pages/mainBar/main__courses/Courses.tsx
+++ client/src/pages/mainBar/main__courses/Courses.tsx
@@ -1,116 +1,13 @@
+import React from "react";
+import DataDisplay from "../encoloure/Enclosure"; 
 
-
-const Courses = () => {
-  return (
-    <div className="container">
-    <div className="container__wrapper">
-
-        <div className="blockA">
-         <span className="blockA__Sback"></span>
-         <span className="blockA__Sfrwd"></span>
-
-         <h2 className="blockA__h2">קורסים קרובים</h2>
-         <div className="blockA__wrapperBlock">
-         <div className="a">its aaa</div>
-         <div className="b">ada</div>
-         <div className="c">ada</div>
+const LectureMain: React.FC = () => {
+    return (
+        <div>
+            <DataDisplay dataType="courses" /> 
         </div>
-        </div>
+    );
+  };
   
-        <div className="blockA">
-        <span className="blockA__Sback"></span>
-         <span className="blockA__Sfrwd"></span>
-         <h2 className="blockA__h2">קורסים עם תעודה</h2>
-         <div className="blockA__wrapperBlock">
-         <div className="a">its aaa</div>
-         <div className="b">ada</div>
-         <div className="c">ada</div>
-        </div>
-        </div>
-
-        <div className="blockA">
-        <span className="blockA__Sback"></span>
-         <span className="blockA__Sfrwd"></span>
-         <h2 className="blockA__h2">השתלמויות</h2>
-         <div className="blockA__wrapperBlock">
-         <div className="a">its aaa</div>
-         <div className="b">ada</div>
-         <div className="c">ada</div>
-        </div>
-        </div>
-        
-        <div className="blockA">
-        <span className="blockA__Sback"></span>
-         <span className="blockA__Sfrwd"></span>
-         <h2 className="blockA__h2">קורסים דיגיטליים</h2>
-         <div className="blockA__wrapperBlock">
-         <div className="a">its aaa</div>
-         <div className="b">ada</div>
-         <div className="c">ada</div>
-        </div>
-        </div>
-
-        <div className="blockA">
-        <span className="blockA__Sback"></span>
-         <span className="blockA__Sfrwd"></span>
-         <h2 className="blockA__h2">קורסים פרונטליים</h2>
-         <div className="blockA__wrapperBlock">
-         <div className="a">its aaa</div>
-         <div className="b">ada</div>
-         <div className="c">ada</div>
-        </div>
-        </div>
-
-        <div className="blockA">
-        <span className="blockA__Sback"></span>
-         <span className="blockA__Sfrwd"></span>
-         <h2 className="blockA__h2">קורסיים מוזלים, הרשמה מאוחרת</h2>
-         <div className="blockA__wrapperBlock">
-         <div className="a">its aaa</div>
-         <div className="b">ada</div>
-         <div className="c">ada</div>
-        </div>
-        </div>
-
-        <div className="blockA">
-        <span className="blockA__Sback"></span>
-         <span className="blockA__Sfrwd"></span>
-         <h2 className="blockA__h2">אסור לפספס</h2>
-         <div className="blockA__wrapperBlock">
-         <div className="a">its aaa</div>
-         <div className="b">ada</div>
-         <div className="c">ada</div>
-        </div>
-        </div>
-
-        <div className="blockA">
-        <span className="blockA__Sback"></span>
-         <span className="blockA__Sfrwd"></span>
-         <h2 className="blockA__h2">קורסים לפי נושא </h2>
-         <div className="blockA__wrapperBlock">
-         <div className="a">its aaa</div>
-         <div className="b">ada</div>
-         <div className="c">ada</div>
-        </div>
-        </div>
-
-        <div className="blockA">
-        <span className="blockA__Sback"></span>
-         <span className="blockA__Sfrwd"></span>
-         <h2 className="blockA__h2"> קורסים חינמיים</h2>
-         <div className="blockA__wrapperBlock">
-         <div className="a">its aaa</div>
-         <div className="b">ada</div>
-         <div className="c">ada</div>
-        </div>
-        </div>
+  export default LectureMain;
   
-  
-  </div>
-  
-   
- </div>
-  )
-}
-
-export default Courses
