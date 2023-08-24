5319d94789c76ddd2e61d94b8080de5216c14551
itsik-ba <112100680+itsik-ba@users.noreply.github.com>
2023-08-24 18:51:03 +0300

    i

--- server.ts
+++ server.ts
@@ -1,60 +1,60 @@
-// server.ts
-// import express, { Request, Response } from 'express';
-// import cors from 'cors';
-// import bodyParser from 'body-parser';
-// import nodemailer from 'nodemailer';
 
-// const app = express();
-// const PORT = 3000;
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import nodemailer from 'nodemailer';
 
-// app.use(cors());
-// app.use(bodyParser.json());
+const app = express();
+const PORT = 3000;
 
-// const transporter = nodemailer.createTransport({
-//   host: 'smtp.gmail.com',
-//   port: 587,
-//   secure: false,
-//   auth: {
-//     user: 'MyLecturesProject2023@gmail.com',
-//     pass: 'Itsik1234Esti',
-//   },
-// });
+app.use(cors());
+app.use(bodyParser.json());
 
-// interface ForgotPasswordRequest {
-//   email: string;
-// }
+const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
+  auth: {
+    user: 'MyLecturesProject2023@gmail.com',
+    pass: 'Itsik1234Esti',
+  },
+});
 
-// interface ForgotPasswordResponse {
-//   message: string;
-// }
+interface ForgotPasswordRequest {
+  email: string;
+}
 
-// app.post('/forgot-password', (req: Request<{}, {}, ForgotPasswordRequest>, res: Response<ForgotPasswordResponse>) => {
-//   const { email } = req.body;
+interface ForgotPasswordResponse {
+  message: string;
+}
 
-//   const resetLink = `https://your-app.com/reset-password?email=${email}`;
+app.post('/forgot-password', (req: Request<{}, {}, ForgotPasswordRequest>, res: Response<ForgotPasswordResponse>) => {
+  const { email } = req.body;
 
-//   const mailOptions = {
-//     from: 'MyLecturesProject2023@gmail.com',
-//     to: email,
-//     subject: 'איפוס סיסמה',
-//     html: `
-//       <p>בקשתך לאיפוס סיסמה.</p>
-//       <p>הקש על הלינק המצורף לאיפוס סיסמה:</p>
-//       <a href="${resetLink}">${resetLink}</a>
-//     `,
-//   };
+  const resetLink = `https://your-app.com/reset-password?email=${email}`;
 
-//   transporter.sendMail(mailOptions, (error, info) => {
-//     if (error) {
-//       console.error('Error sending email:', error);
-//       res.status(500).json({ message: 'Failed to send email' });
-//     } else {
-//       console.log('Email sent:', info.response);
-//       res.json({ message: 'Email sent successfully' });
-//     }
-//   });
-// });
+  const mailOptions = {
+    from: 'MyLecturesProject2023@gmail.com',
+    to: email,
+    subject: 'איפוס סיסמה',
+    html: `
+      <p>בקשתך לאיפוס סיסמה.</p>
+      <p>הקש על הלינק המצורף לאיפוס סיסמה:</p>
+      <a href="${resetLink}">${resetLink}</a>
+    `,
+  };
 
-// app.listen(PORT, () => {
-//   console.log(`Server listening on port ${PORT}`);
-// });
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error('Error sending email:', error);
+      res.status(500).json({ message: 'Failed to send email' });
+    } else {
+      console.log('Email sent:', info.response);
+      res.json({ message: 'Email sent successfully' });
+    }
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
